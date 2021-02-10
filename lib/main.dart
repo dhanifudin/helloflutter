aa2500dfc1456a9a010af1b41e24edb694acec02
Dian Hanifudin Subhi <dhanifudin@gmail.com>
2021-02-10 14:59:15 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Pemrograman Mobile Flutter'),
+      home: MyHomePage(title: 'Pemrograman Mobile Flutter 2F'),
     );
   }
 }
